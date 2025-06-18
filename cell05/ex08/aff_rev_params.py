b181bd76aaea89ae6e8f1a4185e270234a53cd66
ton17278 <tontonko123456789@gmail.com>
2025-06-18 06:07:42 +0000

    go 58

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,10 @@
+import sys
+def main():
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print("none")
+    else:
+        for arg in reversed(args):
+            print(arg)
+if __name__ == "__main__":
+    main()
